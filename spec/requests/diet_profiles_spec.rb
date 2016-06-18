14c08547b28523aedfc1f87336dd04f818102ea9
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-18 20:04:09 +0100

    added tests for response format

--- spec/requests/diet_profiles_spec.rb
+++ spec/requests/diet_profiles_spec.rb
@@ -8,12 +8,28 @@ RSpec.describe "DietProfiles", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it 'returns all of the diet profiles' do
-      FactoryGirl.create_list :diet_profile, 5
-      get diet_profiles_path
-      parsed_response = JSON.parse response.body
-      p parsed_response
-      expect(parsed_response.length).to eq(5)
+    context 'diet profiles exist' do
+      before { FactoryGirl.create_list :diet_profile, 5 }
+
+      it 'returns all of the diet profiles' do
+        get diet_profiles_path
+        parsed_response = JSON.parse response.body
+        expect(parsed_response.length).to eq(5)
+      end
+
+      it 'returns id' do
+        get diet_profiles_path
+        parsed_response = JSON.parse response.body
+        expect(parsed_response.first).to have_key 'id'
+      end
+
+      it 'returns name' do
+        get diet_profiles_path
+        parsed_response = JSON.parse response.body
+        expect(parsed_response.first).to have_key 'name'
+      end
+
     end
+
   end
 end
