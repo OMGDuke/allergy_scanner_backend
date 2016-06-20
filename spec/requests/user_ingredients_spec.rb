6412716f71f7f5af9697fa213eecd324e9648bdc
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-20 14:32:07 +0100

    added test for adding user_ingredient records

--- spec/requests/user_ingredients_spec.rb
+++ spec/requests/user_ingredients_spec.rb
@@ -2,23 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "UserIngredients", type: :request do
 
+  let(:path) { '/user_ingredients' }
   let(:user) { FactoryGirl.create :user }
   let(:ingredients) { FactoryGirl.create_list :ingredient, 5 }
   let(:ingredient_ids) { [] }
+  let(:valid_request) { { user_id: user.id, ingredients: ingredient_ids } }
+
+
 
 
   describe "POST /user_ingredients" do
+
     context 'correct format' do
-      it "returns a successful json string with success message" do
+
+      before do
         ingredients.each { |i| ingredient_ids.push i.id }
-        post '/user_ingredients', { user_id: user.id, ingredients: ingredient_ids }
+      end
+
+      it "returns a successful json string with success message" do
+        post path, valid_request
         expect(response).to be_success
         expect(parse_json_response['success']).to eq 'Ingredients saved to user profile'
       end
 
-      # it 'creates the record in the database' do
+      it 'creates the record in the database' do
+        expect{ post path, valid_request }.to change{ UserIngredient.count }.by 5
+      end
 
-      # end
     end
   end
 
