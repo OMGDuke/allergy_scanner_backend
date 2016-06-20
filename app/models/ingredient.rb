7e58cc400c55ff21c0b511e881f69db4b86fe077
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-20 10:39:37 +0100

    set up link between users and ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,5 @@ class Ingredient < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
   has_and_belongs_to_many :diet_profiles
+  has_and_belongs_to_many :users
 end
