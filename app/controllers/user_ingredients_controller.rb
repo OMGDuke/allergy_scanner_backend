d420b9fa9b908bac8e059c5292042d83a9db0b69
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-20 15:50:01 +0100

    updated test data

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -1,14 +1,6 @@
 class UserIngredientsController < ApplicationController
   def create
-    # byebug
-    # user_ingredient_params
-    p user_ingredient_params
     render json: { success: 'Ingredients saved to user profile' }
-
-  end
-
-  def user_ingredient_params
-     params.permit(:ingredients, array: [:ingredient_id])
   end
 
 end
--- spec/requests/user_ingredients_spec.rb
+++ spec/requests/user_ingredients_spec.rb
@@ -6,10 +6,18 @@ RSpec.describe "UserIngredients", type: :request do
   let(:user) { FactoryGirl.create :user }
   let(:ingredients) { FactoryGirl.create_list :ingredient, 5 }
   let(:ingredient_ids) { [] }
-  let(:valid_request) { { user_id: user.id, ingredients: ingredient_ids } }
-
-
-
+  let(:valid_request) do
+      {
+      user_id: user.id,
+      ingredients: [
+        { id: ingredients[0].id },
+        { id: ingredients[1].id },
+        { id: ingredients[2].id },
+        { id: ingredients[3].id },
+        { id: ingredients[4].id }
+      ]
+    }
+  end
 
   describe "POST /user_ingredients" do
 
@@ -20,13 +28,13 @@ RSpec.describe "UserIngredients", type: :request do
       end
 
       it "returns a successful json string with success message" do
+        byebug
         post path, valid_request
         expect(response).to be_success
         expect(parse_json_response['success']).to eq 'Ingredients saved to user profile'
       end
 
       it 'creates the record in the database' do
-        # p valid_request
         expect{ post path, valid_request }.to change{ UserIngredient.count }.by 5
       end
 
